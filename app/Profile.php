39880fe37d22b5280741d71cc02f94f2fea6299d
Zoran Damjanov <zoolinom@gmail.com>
2020-03-24 13:32:14 +0100

    Inverse of One to one relation

--- app/Profile.php
+++ app/Profile.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    //
+    public function user()
+    {
+        // profile belongs to one user (inverse of one to one relation)
+        return $this->belongsTo(User::class);
+    }
 }
